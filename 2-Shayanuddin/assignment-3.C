4a847da3bb078b3f8d4a83f0a9f78d5d2cf38462
MdShayanuddin <mohd95shayanuddin@gmail.com>
2024-11-22 23:15:16 +0530

    My Task Is Completed

---
+++ 2-Shayanuddin/assignment-3.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    float num1, num2, num3, sum, average;
+
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+    
+    printf("Enter the third number: ");
+    scanf("%f", &num3);
+
+    sum = num1 + num2 + num3;
+
+    average = sum / 3;
+
+    printf("The sum of the three numbers is: %.2f\n", sum);
+    printf("The average of the three numbers is: %.2f\n", average);
+
+    return 0;
+
+}
